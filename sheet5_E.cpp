2ccc2686922b7c148dc6a04659ba28982756b674
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-10-20 23:09:14 +0600

    Create sheet5_E.cpp

---
+++ sheet5_E.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swapp(int a,int b)
+{
+    swap(a,b);
+
+    cout<<a<<" "<<b;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    swapp(a,b);
+
+
+    return 0;
+}
